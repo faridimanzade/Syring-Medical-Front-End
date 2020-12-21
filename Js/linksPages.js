256918945a88b5255aff8648f78869cb7bd2dfd0
faridimanzade <faridfi@code.edu.az>
2020-12-21 17:19:36 +0400

    Bug Fix On Footers

---
+++ Js/linksPages.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+
+    // ====================================== LINKS FOR NAVIGATION
+    $(".dropdown-item").eq(0).click(function () {
+        window.open('home.html', "_self");
+    });
+
+    $(".dropdown-item").eq(15).click(function () {
+        window.open('contactUs.html', "_self");
+    });
+
+    $(".dropdown-item").eq(13).click(function () {
+        window.open('ourClinic.html', "_self");
+    });
+
+    $(".dropdown-item").eq(16).click(function () {
+        window.open('pagesFaq.html', "_self");
+    });
+
+    $(".dropdown-item").eq(21).click(function () {
+        window.open('pricing.html', "_self");
+    });
+
+    $(".dropdown-item").eq(25).click(function () {
+        window.open('findDoctor.html', "_self");
+    });
+
+    $(".dropdown-item").eq(26).click(function () {
+        window.open('singleDoctor.html', "_self");
+    });
+
+    $(".dropdown-item").eq(27).click(function () {
+        window.open('medicalDepartment.html', "_self");
+    });
+
+    $(".dropdown-item").eq(28).click(function () {
+        window.open('dentalClinic.html', "_self");
+    });
+
+    $(".dropdown-item").eq(52).click(function () {
+        window.open('chat.html', "_self");
+    });
+
+    $(".dropdown-item").eq(57).click(function () {
+        window.open('shop.html', "_self");
+    });
+
+    $(".dropdown-item").eq(60).click(function () {
+        window.open('Cart.html', "_self");
+    });
+
+    $(".dropdown-item").eq(61).click(function () {
+        window.open('checkout.html', "_self");
+    });
+    $(".logo img").click(function () {
+        window.open('home.html', "_self");
+    });
+
+
+
+    // ====================================== LINKS FOR BURGER MENU
+
+    $(".burgerMenuSubNavNameSelf p").eq(0).click(function(){
+        window.open('home.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(14).click(function(){
+        window.open('contactUs.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(12).click(function(){
+        window.open('ourClinic.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(15).click(function(){
+        window.open('pagesFaq.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(19).click(function(){
+        window.open('pricing.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(22).click(function(){
+        window.open('findDoctor.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(23).click(function(){
+        window.open('singleDoctor.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(24).click(function(){
+        window.open('medicalDepartment.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(25).click(function(){
+        window.open('dentalClinic.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(44).click(function(){
+        window.open('chat.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(49).click(function(){
+        window.open('shop.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(52).click(function(){
+        window.open('Cart.html', "_self");
+    });
+
+    $(".burgerMenuSubNavNameSelf p").eq(53).click(function(){
+        window.open('checkout.html', "_self");
+    });
+
+
+});
